ed1de411eed7a3aa4a0c8372a775e4ea7d7424cd
Henning H. Stephansen <henningms@gmail.com>
2011-10-22 05:27:53 +0200

    add Async-methods for Travelstage API
    add Async-methods for Trip API

---
+++ trafikantendotnet-wp7/Travelstages/Travelstages.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Trafikanten.Common;
+using Trafikanten.Common.QueryBuilder;
+using Trafikanten.Common.QueryBuilder.Travelstage;
+using Trafikanten.Travels;
+
+namespace Trafikanten.Travelstages
+{
+    public class Travelstages
+    {
+        public static void GetTravelstageCollectionAsync(IQueryBuilder url, TravelstageCollectionDelegate callback)
+        {
+            try
+            {
+                if (url == null) throw new Exception("Cannot work with null-objects");
+                if (String.IsNullOrEmpty(url.Url)) throw new Exception("Url cannot be empty");
+
+                var client = new WebClient();
+
+                client.DownloadStringCompleted += (s, e) =>
+                {
+                    if (e.Error != null) throw e.Error;
+                    if (e.Result == null) return;
+
+                    var collection = Json.JsonHelper.Deserialize<IList<Travelstage>>(e.Result);
+
+                    callback(new ObservableCollection<Travelstage>(collection));
+                };
+
+                client.DownloadStringAsync(new Uri(url.Url));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void GetArrivalsAsync(GetArrivalsQueryBuilder url, TravelstageCollectionDelegate callback)
+        {
+            GetTravelstageCollectionAsync(url, callback);
+        }
+
+        public static void GetArrivalsAdvancedAsync(GetArrivalsAdvancedQueryBuilder url, TravelstageCollectionDelegate callback)
+        {
+            GetTravelstageCollectionAsync(url, callback);
+        }
+
+        public static void GetDeparturesAsync(GetDeparturesQueryBuilder url, TravelstageCollectionDelegate callback)
+        {
+            GetTravelstageCollectionAsync(url, callback);
+        }
+
+        public static void GetDeparturesAdvancesAsync(GetDeparturesAdvancedQueryBuilder url, TravelstageCollectionDelegate callback)
+        {
+            GetTravelstageCollectionAsync(url, callback);
+        }
+    }
+}
---
+++ trafikantendotnet-wp7/Trips/Trips.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Trafikanten.Common;
+using Trafikanten.Common.QueryBuilder.Trip;
+using Trafikanten.Json;
+using Trafikanten.Places;
+
+namespace Trafikanten.Trips
+{
+    public class Trips
+    {
+        public static void GetTripAsync(GetTripQueryBuilder url, TripDelegate callback)
+        {
+            try
+            {
+                if (url == null) throw new Exception("Cannot work with null-objects");
+                if (String.IsNullOrEmpty(url.Url)) throw new Exception("Url cannot be empty");
+
+                var client = new WebClient();
+
+                client.DownloadStringCompleted += (s, e) =>
+                {
+                    if (e.Error != null) throw new Exception("", e.Error);
+                    if (e.Result == null) return;
+
+                    var collection = JsonHelper.Deserialize<Trip>(e.Result);
+
+                    callback(collection);
+                };
+
+                client.DownloadStringAsync(new Uri(url.Url));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
